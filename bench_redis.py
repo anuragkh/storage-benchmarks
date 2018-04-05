7c96f2bd8482faa6ff91368ca8d0278212eed219
Anurag Khandelwal <anuragk@berkeley.edu>
2018-04-05 10:50:59 -0700

    Added benchmark scripts for S3 and redis

---
+++ bench_redis.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import sys
+
+from benchmark.redis_benchmark import *
+
+
+def run_benchmark(host, port):
+    workload_paths = ['load', 'workloada', 'workloadb', 'workloadc', 'workloadd']
+    tot_ops = 1000000
+    n_ops = 100000
+    for workload_path in workload_paths:
+        workload_off = 0
+        for n_proc in [1, 2, 4, 8, 16, 32]:
+            logging.info(
+                "Benchmarking sync throughput for workload_path=%s and num_thread=%d" % (workload_path, n_proc))
+            output_file = workload_path + "-throughput-" + str(n_proc) + ".txt"
+            stdout_backup = sys.stdout
+            with open(output_file, 'w') as f:
+                sys.stdout = f
+                redis_bench_throughput(host, port, workload_path, n_ops, n_proc)
+            sys.stdout = stdout_backup
+            workload_off += n_ops
+        logging.info("Benchmarking sync latency for workload path %s" % workload_path)
+        latency_ops = tot_ops - workload_off if workload_path.endswith('/load') else n_ops
+        output_file = workload_path + "-latency.txt"
+        stdout_backup = sys.stdout
+        with open(output_file, 'w') as f:
+            sys.stdout = f
+            redis_bench_latency(host, port, workload_path, workload_off, latency_ops)
+        sys.stdout = stdout_backup
+
+    redis_clear(host, port)
+
+    n_proc = 1
+    for workload_path in workload_paths:
+        workload_off = 0
+        for max_async in [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]:
+            logging.info(
+                "Benchmarking async throughput for workload_path=%s and max_async=%d" % (workload_path, max_async))
+            output_file = workload_path + "_async-throughput-" + str(max_async) + ".txt"
+            stdout_backup = sys.stdout
+            with open(output_file, 'w') as f:
+                sys.stdout = f
+                redis_bench_pipelined_throughput(host, port, workload_path, workload_off, n_ops, n_proc, max_async)
+            sys.stdout = stdout_backup
+            workload_off += n_ops
+    redis_clear(host, port)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Benchmark Redis operations.')
+    parser.add_argument('--host', type=str, nargs=1, default='127.0.0.1', help='Redis host')
+    parser.add_argument('--port', type=int, nargs=1, default=6379, help='Redis port')
+    args = parser.parse_args()
+    run_benchmark(args.host, args.port)
---
+++ bench_s3.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import argparse
+import logging
+import sys
+
+from benchmark.s3_benchmark import *
+
+
+def run_benchmark(bucket):
+    workload_paths = ['load', 'workloada', 'workloadb', 'workloadc', 'workloadd']
+    tot_ops = 1000000
+    n_ops = 100000
+    for workload_path in workload_paths:
+        workload_off = 0
+        for n_proc in [1, 2, 4, 8, 16, 32]:
+            logging.info(
+                "Benchmarking sync throughput for workload_path=%s and num_thread=%d" % (workload_path, n_proc))
+            output_file = workload_path + "-throughput-" + str(n_proc) + ".txt"
+            stdout_backup = sys.stdout
+            with open(output_file, 'w') as f:
+                sys.stdout = f
+                s3_bench_throughput(bucket, workload_path, n_ops, n_proc)
+            sys.stdout = stdout_backup
+            workload_off += n_ops
+        logging.info("Benchmarking sync latency for workload path %s" % workload_path)
+        latency_ops = tot_ops - workload_off if workload_path.endswith('/load') else n_ops
+        output_file = workload_path + "-latency.txt"
+        stdout_backup = sys.stdout
+        with open(output_file, 'w') as f:
+            sys.stdout = f
+            s3_bench_latency(bucket, workload_path, workload_off, latency_ops)
+        sys.stdout = stdout_backup
+
+    s3_clear(bucket)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Benchmark S3 operations.')
+    parser.add_argument('--bucket', type=str, nargs=1, required=True, help='S3 bucket name')
+    args = parser.parse_args()
+    run_benchmark(args.bucket)
