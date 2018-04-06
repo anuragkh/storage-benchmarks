a26c37eaf6c40ea930cd47f6b9c128e93607fd88
Anurag Khandelwal <anuragk@berkeley.edu>
2018-04-05 21:23:46 -0700

    Fix load

--- benchmark/workload.py
+++ benchmark/workload.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO,
 def make_workload(path, off, count, client):
     logging.info("Loading %d entries from %s from offset %d" % (count, path, off))
     with open(path) as f:
-        ops = [x.strip().split() for x in f.readlines()[off:(off + count)]]
+        ops = [x.strip().split() for x in f.readlines()[int(off):int(off + count)]]
         workload = [[getattr(client, x[0]), x[1:]] for x in ops]
 
     logging.info("Loaded %d entries from %s from offset %d" % (len(workload), path, off))
